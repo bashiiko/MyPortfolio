9caecf677c7a16ff83213130e5b9f5cec9c4feab
bashiiko <bashiko.dev@gmail.com>
2019-12-31 02:02:05 +0900

    [update] 本番環境用にSTATIC_ROOT, STATICFILES_DIRSを設定

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(APP_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'portfolio/static/'),
+)
 STATIC_URL = '/static/'
 
 if not DEBUG:
